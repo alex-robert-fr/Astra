0e0e1fadf168fabb8575afb8fe0b0ea82ef5658c
Alex Robert <contact.alexrobert04@gmail.com>
2024-06-18 13:35:05 +0200

    feat: add index.html + route url

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-import express from "express"
+const express = require("express")
+const path = require("path")
 
 const app = express()
 const port = 3000
 
 app.get("/", (_req, res) => {
-  res.send("Hello World!")
+  res.sendFile(path.join(__dirname, "src/front/index.html"))
 })
 
 app.listen(port, () => {
